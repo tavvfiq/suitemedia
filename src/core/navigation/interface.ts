c29a248708379a569f10667676345012ee40d497
tavvfiq <taufiq.w@mail.ugm.ac.id>
2022-02-08 00:24:57 +0700

    delete unused dependency

--- src/core/navigation/interface.ts
+++
@@ -1,6 +0,0 @@
-import { Container } from 'inversify';
-import { NavigationFunctionComponent } from 'react-native-navigation';
-
-export interface NavigationWithDI extends NavigationFunctionComponent {
-  container: Container;
-}
